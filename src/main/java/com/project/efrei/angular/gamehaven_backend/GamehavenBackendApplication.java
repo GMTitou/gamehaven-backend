5196981ac39d9681f00ad71f3afed0a8188adf5b
GMTitou <pochebonnebaptiste@gmail.com>
2024-11-04 17:17:18 +0100

    Initial commit

---
+++ src/main/java/com/project/efrei/angular/gamehaven_backend/GamehavenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.efrei.angular.gamehaven_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehavenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamehavenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/efrei/angular/gamehaven_backend/GamehavenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.efrei.angular.gamehaven_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehavenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
